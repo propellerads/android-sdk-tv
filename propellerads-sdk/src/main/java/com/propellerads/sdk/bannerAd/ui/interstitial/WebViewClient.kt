5d7a671bd9b1648be8041df063fe9324a94cc5ab
Mikhail Sharin <mik.sharin@gmail.com>
2022-04-23 13:21:11 +0400

    Improve WebView HTTP errors handling

--- propellerads-sdk/src/main/java/com/propellerads/sdk/bannerAd/ui/interstitial/WebViewClient.kt
+++ propellerads-sdk/src/main/java/com/propellerads/sdk/bannerAd/ui/interstitial/WebViewClient.kt
@@ -15,28 +15,47 @@ class WebViewClient(
     private val onLandingClicked: (uri: String?) -> Unit,
 ) : WebViewClient() {
 
+    private companion object {
+        const val TAG = "WebView"
+    }
+
     override fun onPageFinished(view: WebView?, url: String?) {
+        Logger.d("Page finished", TAG)
         onPageFinished()
     }
 
 
+    // This works on API 23 and above.
+    // No way to determine an http error in previous API versions.
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceivedHttpError(
-        view: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?
+        webView: WebView?, request: WebResourceRequest?, errorResponse: WebResourceResponse?
     ) {
-        // This works on API 23 and above.
-        // No way to determine an http error in previous API versions.
-        onPageFailed()
+        val url = webView?.url?.dropLastWhile { it == '#' }
+        val originalUrl = webView?.originalUrl?.dropLastWhile { it == '#' }
+        val failedUrl = request?.url?.toString()?.dropLastWhile { it == '#' } ?: ""
+        val isPageFailed = failedUrl.isNotBlank() &&
+                ((!url.isNullOrBlank() && failedUrl == url) ||
+                        (!originalUrl.isNullOrBlank() && failedUrl == originalUrl))
+        // Invoke the fail callback only if the main resource loading is failed
+        if (isPageFailed) {
+            val message = "${errorResponse?.statusCode} ${errorResponse?.reasonPhrase}"
+            Logger.d("Page failed: HTTP error: $message", TAG)
+            onPageFailed()
+        }
     }
 
     override fun onReceivedError(
         view: WebView?, request: WebResourceRequest?, error: WebResourceError?
     ) {
+        Logger.d("Page failed: Network error", TAG)
         onPageFailed()
     }
 
     override fun onReceivedSslError(
         view: WebView?, handler: SslErrorHandler?, error: SslError?
     ) {
+        Logger.d("Page failed: SSL error", TAG)
         onPageFailed()
     }
 
@@ -65,7 +84,7 @@ class WebViewClient(
         uri: Uri?,
         hasGesture: Boolean?
     ): Boolean {
-        Logger.d("WebView redirect. Has gesture: $hasGesture; Url: $uri")
+        Logger.d("WebView redirect. Has gesture: $hasGesture; Url: $uri", TAG)
 
         if (hasGesture == true) {
             onLandingClicked(uri?.toString())
